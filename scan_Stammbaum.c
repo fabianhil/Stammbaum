4f298442053480bf571ece962a5000b6377c4cec
Fabian Hilbich <fabian_hilbich@web.de>
2022-09-05 23:17:41 +0200

    Programm für Aufgabe 10 Stammbaum angepasst.
    Struktur für Stammbaum angelegt.
    Print Funktion angepasst.
    Stammbaum.csv zum einlesen erstellt.
    
    Programm getestet.

---
+++ scan_Stammbaum.c
@@ -0,0 +1,169 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char const* const VERSIONSNR = "0.1.2";
+
+int const DB_SIZE = 50;
+
+struct vorfahren
+{
+	char nachname[20];
+	char vorname[20];
+};
+
+struct datum
+{
+	int jahr;
+	int monat;
+	int tag;
+};
+
+struct kinder
+{
+	char nachname[20];
+	char vorname[20];
+};
+
+struct partner
+{
+	char nachname[20];
+	char vorname[20];
+};
+
+struct person
+{
+    int  generation;
+    struct vorfahren vorfahre;
+	char nachname[20];
+    char vorname[20];
+    struct datum geb_datum;
+	struct partner ehe_partner;
+	struct kinder kind1;
+	struct kinder kind2;
+	struct kinder kind3;
+	struct kinder kind4;
+};
+typedef struct person person_t;
+
+void readcsv(char const* const datei)
+{
+    FILE* filepointer = NULL;
+    int   zaehler     = 0;
+
+    filepointer = fopen(datei, "r");
+    if(NULL == filepointer)
+    {
+        fprintf(stderr, "Couldnt open file '%s'\n", datei);
+        exit(2);
+    }
+    person_t stammbaum[DB_SIZE];
+    while(fscanf(filepointer,
+                 "%d,%[^,],%[^,],%[^,],%[^,],%d,%d,%d,%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%s",
+                 &stammbaum[zaehler].generation,
+                 stammbaum[zaehler].vorfahre.nachname,
+                 stammbaum[zaehler].vorfahre.vorname,
+				 stammbaum[zaehler].nachname,
+                 stammbaum[zaehler].vorname,
+                 &stammbaum[zaehler].geb_datum.jahr,
+				 &stammbaum[zaehler].geb_datum.monat,
+				 &stammbaum[zaehler].geb_datum.tag,
+				 stammbaum[zaehler].ehe_partner.nachname,
+                 stammbaum[zaehler].ehe_partner.vorname,
+				 stammbaum[zaehler].kind1.nachname,
+                 stammbaum[zaehler].kind1.vorname,
+				 stammbaum[zaehler].kind2.nachname,
+                 stammbaum[zaehler].kind2.vorname,
+				 stammbaum[zaehler].kind3.nachname,
+                 stammbaum[zaehler].kind3.vorname,
+				 stammbaum[zaehler].kind4.nachname,
+                 stammbaum[zaehler].kind4.vorname)
+          != EOF)
+    {
+        printf("\nGeneration: %d \n Vorfahre: %s, %s \n Name: %s, %s \n Geburtsdatum: %d.%d.%d \n Partner: %s, %s \n",
+				stammbaum[zaehler].generation,
+                stammbaum[zaehler].vorfahre.nachname,
+                stammbaum[zaehler].vorfahre.vorname,
+				stammbaum[zaehler].nachname,
+                stammbaum[zaehler].vorname,
+                stammbaum[zaehler].geb_datum.tag,
+				stammbaum[zaehler].geb_datum.monat,
+				stammbaum[zaehler].geb_datum.jahr,
+				stammbaum[zaehler].ehe_partner.nachname,
+                stammbaum[zaehler].ehe_partner.vorname);
+			if(stammbaum[zaehler].kind1.vorname[1]!= '%')
+			{
+				printf(" Kind 1: %s, %s \n",
+				stammbaum[zaehler].kind1.nachname,
+				stammbaum[zaehler].kind1.vorname);
+			}
+			if(stammbaum[zaehler].kind2.vorname[1]!= '%')
+			{
+				printf(" Kind 2: %s, %s \n",
+				stammbaum[zaehler].kind2.nachname,
+				stammbaum[zaehler].kind2.vorname);
+			}
+			if(stammbaum[zaehler].kind3.vorname[1]!= '%')
+			{
+				printf(" Kind 3: %s, %s \n",
+				stammbaum[zaehler].kind3.nachname,
+				stammbaum[zaehler].kind3.vorname);
+			}
+			if(stammbaum[zaehler].kind4.vorname[0]!= '%')
+			{
+				printf(" Kind 4: %s, %s \n",
+				stammbaum[zaehler].kind4.nachname,
+				stammbaum[zaehler].kind4.vorname);
+			}
+        zaehler++;
+        if(zaehler == DB_SIZE)
+        {
+            fprintf(stderr, "Datenbank voll! \n");
+            break;
+        }
+    }
+    // check if file closed correctly
+    if(fclose(filepointer) == EOF)
+    {
+        fprintf(stderr, "Fehler beim schließen der Datei! \n");
+        exit(2);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        fprintf(stderr, "No option recognized. Wrong Usage. Please try -h\n");
+    }
+
+    while(true)
+    {
+        int option = getopt(argc, argv, "hvf:");
+        switch(option)
+        {
+        case 'h':
+            printf("Bitte -f nutzen um einen File-Path anzugeben\n");
+            exit(0);
+
+        case 'v':
+            printf("Version %s\n", VERSIONSNR);
+            exit(0);
+
+        case 'f':
+            readcsv(optarg);
+            break;
+
+        case '?':
+            fprintf(stderr, "Please try -h\n");
+            exit(1);
+
+        case -1:
+            exit(0);
+        }
+    }
+
+    return 0;
+}
